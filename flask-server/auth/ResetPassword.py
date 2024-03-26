3172d5b7a722bcfb382892cfa6140c6e20fb12d4
euler1729 <mahmudul.hhh@gmail.com>
2024-03-26 21:22:43 +0600

    auth done

--- flask-server/auth/ResetPassword.py
+++ flask-server/auth/ResetPassword.py
@@ -2,7 +2,8 @@ from flask_restful import Resource
 from flask import request, jsonify, make_response, Response
 from sendmail import send_otp
 import sqlite3
-
+import time
+from werkzeug.security import generate_password_hash
 
 class ResetPasswordInit(Resource):
     def post(self):
@@ -23,5 +24,41 @@ class ResetPasswordInit(Resource):
         except Exception as e:
             return make_response(jsonify({'msg':'An error occurred'}), 500)
 class ResetPasswordConfirm(Resource):
+    
     def post(self):
-        return make_response(jsonify({'msg':'Reset Password confirm- post'}), 200)
+        # Get request parameters
+        data = request.get_json()
+        user_name = data['user_name']
+        otp = data['otp']
+        new_password = data['new_password']
+        new_password_hash = generate_password_hash(new_password)
+
+        # Connect to the database
+        conn = sqlite3.connect('sqlite.db')
+        cursor = conn.cursor()
+
+        # Check if username or email exists in the user table
+        cursor.execute("SELECT user_id FROM user WHERE user_name = ? OR email = ?", (user_name, user_name))
+        user_row = cursor.fetchone()
+
+        if user_row:
+            user_id = user_row[0]
+            # Check if the OTP matches for the user
+            cursor.execute("SELECT * FROM otp WHERE user_id = ? AND otp = ?", (user_id, otp,))
+            otp_row = cursor.fetchone()
+
+            if otp_row:
+                if otp_row[2] < int(time.time() * 1000):
+                    conn.close()
+                    return make_response(jsonify({'msg': 'OTP expired'}), 400)
+                cursor.execute("UPDATE user SET password = ? WHERE user_id = ?", (new_password_hash, user_id,))
+                cursor.execute("DELETE FROM otp WHERE user_id = ?", (user_id,))
+                conn.commit()
+                conn.close()
+                return make_response(jsonify({'msg': 'Password updated successfully'}), 200)
+            else:
+                conn.close()
+                return make_response(jsonify({'msg': 'Invalid OTP'}), 400)
+        else:
+            conn.close()
+            return make_response(jsonify({'msg': 'User not found'}), 404)
